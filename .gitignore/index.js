7bc88fd5bf1f2807af2d337ee3b41ae7d3f56eaf
iplay89 <45695205+iplay89@users.noreply.github.com>
2018-12-07 18:39:46 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,23 @@
 const Discord = require("discord.js");
 
+var PREFIX = "&";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("Atonservix, &aide");
     console.log("Atonservix / le bot demarre")
 });
+
+bot.on('message', message => {
+    if(message.content[0] === PREFIX) {
+        if(message.content === '&aide') {
+            message.channel.send({embed: {
+                color: 3447003,
+                decription: 'test'
+            }});
+        }
+    }
+}
 
 bot.login(process.env.TOKEN);
